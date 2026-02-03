2d56e05b2d82fa118e0f07764057e045e35ff8a8
Aditi Garg <aditi.garg2@globallogic.com>
2026-02-03 10:27:12 +0530

    Test sum function

--- Episode13/Foodify/src/components/Body.js
+++ Episode13/Foodify/src/components/Body.js
@@ -19,9 +19,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(resMenuList);
-      // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    
+    const data = await fetch(resMenuList);    
     const jsonData = await data.json();
     setResList(
       //optional chaining
